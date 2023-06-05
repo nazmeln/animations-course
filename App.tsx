069ed8eea8babb9878bc041bf69c5691e7f0da65
Nazarii Melnyk <nazarii.melnyk@kilo.health>
2023-06-05 14:20:50 +0100

    feat: panGestureCard

--- App.tsx
+++ App.tsx
@@ -9,21 +9,13 @@ import React from 'react';
 import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
   useColorScheme,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 import {PanGestureCard} from './src/animations';
 
 type SectionProps = PropsWithChildren<{
@@ -57,48 +49,17 @@ function Section({children, title}: SectionProps): JSX.Element {
 }
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <PanGestureCard />
-        {/* <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View> */}
-      </ScrollView>
+    <SafeAreaView style={styles.container}>
+      <PanGestureCard />
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   sectionContainer: {
     marginTop: 32,
     paddingHorizontal: 24,
--- src/animations/PanGestureCard.tsx
+++ src/animations/PanGestureCard.tsx
@@ -1,13 +1,73 @@
 import React from 'react';
 
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, useWindowDimensions} from 'react-native';
+import {PanGestureHandler} from 'react-native-gesture-handler';
+import Animated, {
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+  useSharedValue,
+  withDecay,
+} from 'react-native-reanimated';
+import {clamp} from 'react-native-redash';
+
+const CARD_HEIGHT = 200;
+const CARD_WIDTH = 300;
+
+const Card = () => {
+  return (
+    <View style={styles.card}>
+      <Text style={styles.cardTitle}>Animated card</Text>
+    </View>
+  );
+};
 
 export const PanGestureCard = () => {
+  const {width, height} = useWindowDimensions();
+
+  const bondX = width - CARD_WIDTH;
+  const bondY = height - CARD_HEIGHT;
+
+  // creating 2 animations values
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
+
+  const onGestureEventHandler = useAnimatedGestureHandler({
+    onStart: (event, ctx) => {
+      //needed in order to keep the card in the same position
+      ctx.offsetX = translateX.value;
+      ctx.offsetY = translateY.value;
+    },
+
+    onActive: (event, ctx) => {
+      translateX.value = clamp(ctx.offsetX + event.translationX, 0, bondX);
+      translateY.value = clamp(ctx.offsetY + event.translationY, 0, bondY);
+    },
+
+    onEnd: (event, ctx) => {
+      translateX.value = withDecay({
+        velocity: event.velocityX,
+        clamp: [0, bondX],
+      });
+      translateY.value = withDecay({
+        velocity: event.velocityY,
+        clamp: [0, bondY],
+      });
+    },
+  });
+
+  // creating animated style
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{translateX: translateX.value}, {translateY: translateY.value}],
+  }));
+
   return (
     <View style={styles.container}>
-      <View style={styles.card}>
-        <Text>Animated card</Text>
-      </View>
+      <PanGestureHandler onGestureEvent={onGestureEventHandler}>
+        <Animated.View style={animatedStyle}>
+          <Card />
+        </Animated.View>
+      </PanGestureHandler>
     </View>
   );
 };
@@ -17,8 +77,14 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   card: {
-    height: 100,
-    width: 300,
+    borderRadius: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: CARD_HEIGHT,
+    width: CARD_WIDTH,
     backgroundColor: 'green',
+  },
+  cardTitle: {
+    color: 'white',
   },
 });
